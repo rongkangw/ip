a3c286d728485ba9dc3472b419d7b4a09b8b237b
rongkangw <138689948+rongkangw@users.noreply.github.com>
2025-08-17 23:18:20 +0800

    Edit and Rename Duke.java to TheCoolerDuke.java

---
+++ src/main/java/TheCoolerDuke.java
@@ -0,0 +1,10 @@
+public class TheCoolerDuke {
+    public static void main(String[] args) {
+        String lineBreak = "_".repeat(72);
+        System.out.println(lineBreak);
+        System.out.println("Hello! I'm TheCoolerDuke");
+        System.out.println("What can I do for you?");
+        System.out.println(lineBreak);
+        System.out.println("I am leaving now. Goodbye!");
+    }
+}
