51669b46dd7e6d7ecd7c8b0aa24180a615bab6e3
rongkangw <138689948+rongkangw@users.noreply.github.com>
2025-08-28 00:07:35 +0800

    Add tests for TaskHistoryManager and Command classes.
    Resources for tests added accordingly.

--- src/test/java/taskFeature/TaskHistoryManagerTest.java
+++ src/test/java/taskFeature/TaskHistoryManagerTest.java
@@ -1,4 +1,44 @@
 package taskFeature;
 
+import org.junit.jupiter.api.Test;
+import taskFeature.tasks.Deadline;
+
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TaskHistoryManagerTest {
+
+    @Test
+    public void testRetrieveTaskHistory() {
+        String pathDir = Paths.get("src", "test", "resources", "TaskHistoryRetrieve.txt").toString();
+        TaskHistoryManager thm = new TaskHistoryManager(pathDir);
+
+        ArrayList<Task> taskList = new ArrayList<>();
+
+        //test file is found
+        assertTrue(thm.retrieveTaskHistory(taskList));
+
+        //1 task retrieved
+        assertEquals(1, taskList.size());
+    }
+
+    @Test
+    public void testUpdateHistory() {
+        String pathDir = Paths.get("src", "test", "resources", "TaskHistoryUpdate.txt").toString();
+        TaskHistoryManager thm = new TaskHistoryManager(pathDir);
+
+        //initialise array of 1 task
+        ArrayList<Task> taskList = new ArrayList<>();
+        LocalDateTime date = LocalDateTime.parse("2007-12-03T10:15:30");
+        taskList.add(new Deadline("getFood", false, date));
+
+        //ensure history updated without error
+        assertDoesNotThrow(() -> thm.updateHistory(taskList));
+
+
+    }
+
 }
--- src/test/java/ui/CommandTest.java
+++ src/test/java/ui/CommandTest.java
@@ -1,4 +1,30 @@
 package ui;
 
+import exceptions.InvalidFormatException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static ui.Command.validateAndFormatModifier;
+
 public class CommandTest {
+    @Test
+    public void testValidCommand() {
+        assertEquals(Command.LIST_TASK, Command.validateCommand("list"));
+    }
+
+    @Test
+    public void testInvalidCommand() {
+        assertNull(Command.validateCommand("ls"));
+    }
+
+    @Test
+    public void testThrowInvalidFormatExceptionOnInvalidFormat() {
+        String modifierString = "test event /by 12/2/2025 18:00";
+        String [] acceptedModifiers = new String[]{"/from","/to"};
+        
+        assertThrowsExactly(
+                InvalidFormatException.class,
+                () -> validateAndFormatModifier(modifierString, acceptedModifiers)
+        );
+    }
 }
